3364c4792cfa2d0435b991d51a1f484946ce8f2d
harendra263 <harendra263@gmail.com>
2023-03-25 11:47:13 +0530

    new data models added

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, flash, request, redirect, url_for, session
-from webforms import LoginForm, RegisterForm
+from webforms import LoginForm, RegisterForm, UserForm, PredictForm
 from flask_bootstrap import Bootstrap
 import pandas as pd
 from werkzeug.utils import secure_filename
@@ -8,6 +8,8 @@ import os
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+
+from src.pipeline.predict import CustomData, PredictPipeline
 
 from datetime import datetime
 
@@ -29,14 +31,50 @@ class User(UserMixin, db.Model):
     password = db.Column(db.String(80))
     joining_date = db.Column(db.DateTime, default = datetime.utcnow)
 
+    def __repr__(self) -> str:
+        return f"<User(username='{self.username}', email='{self.email}')>"
+
+class Predict(db.Model):
+    user_id = db.Column(db.Integer, primary_key=True)
+    Item_Identifier = db.Column(db.String(15), unique=True, nullable=False)
+    Item_Weight = db.Column(db.Numeric(10, 3), nullable=False)
+    Item_Fat_Content = db.Column(db.String(15), nullable=False)
+    Item_Visibility = db.Column(db.Numeric(10, 3), nullable=False)
+    Item_Type = db.Column(db.String(15), nullable=False)
+    Item_MRP = db.Column(db.Numeric(10, 3), nullable=False)
+    Outlet_Identifier = db.Column(db.String(15), unique=True, nullable=False)
+    Outlet_Size = db.Column(db.String(15), nullable=False)
+    Outlet_Location_Type = db.Column(db.String(15), nullable=False)
+    Outlet_Type = db.Column(db.String(15), nullable=False)
+    Prediction = db.Column(db.Numeric(10, 3), nullable=False)
+
+
+class ItemIdentifier(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    item_name = db.Column(db.String(50),unique=True)
+
+
+class StoreIdentifier(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    store_name = db.Column(db.String(50),unique=True)
+
+class FatContent(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    Content = db.Column(db.String(50), unique=True)
+
+class OutletType(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    Outlet_Type = db.Column(db.String(50), unique=True)
+
+class LocationType(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    Location_Type = db.Column(db.String(50), unique=True)
+
+
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
-
-
-
-
-
 
 @app.route('/')
 def home():
@@ -81,3 +119,52 @@ def about():
 def logout():
     logout_user()
     return redirect(url_for('home'))
+
+
+@app.route('/profile', methods = ['GET', 'POST'])
+def profile():
+    form = UserForm()
+    user_id = current_user.id
+    name_to_update = User.query.get_or_404(user_id)
+
+
+@app.route('/predict', methods = ['GET', 'POST'])
+def predict():
+    form = PredictForm()
+    user_id = current_user.id
+    if request.method == "GET":
+        return render_template("home.html")
+    if form.validate_on_submit():
+        data = CustomData(
+            Item_Identifier= form.Item_Identifier.data,
+            Item_Weight= form.Item_Weight.data,
+            Item_Fat_Content= form.Item_Fat_Content.data,
+            Item_Visibility = form.Item_Visibility.data,
+            Item_Type = form.Item_Type.data,
+            Item_MRP = form.Item_MRP.data,
+            Outlet_Identifier= form.Outlet_Identifier.data,
+            Outlet_Type= form.Outlet_Type.data,
+            Outlet_Location_Type= form.Outlet_Location_Type.data,
+            Outlet_Size= form.Outlet_Size
+        )
+        pred_df = data.get_data_as_dataframe()
+        predict_pipeline = PredictPipeline()
+        results = predict_pipeline.predict(pred_df)
+        data_db = Predict(
+            user_id = user_id,
+            Item_Identifier= form.Item_Identifier.data,
+            Item_Weight= form.Item_Weight.data,
+            Item_Fat_Content= form.Item_Fat_Content.data,
+            Item_Visibility = form.Item_Visibility.data,
+            Item_Type = form.Item_Type.data,
+            Item_MRP = form.Item_MRP.data,
+            Outlet_Identifier= form.Outlet_Identifier.data,
+            Outlet_Size= form.Outlet_Size,
+            Outlet_Location_Type= form.Outlet_Location_Type.data,
+            Outlet_Type= form.Outlet_Type.data,
+            Prediction = results
+        )
+        db.session.add(data_db)
+        db.session.commit()
+
+        return render_template('predict.html', results = results[0])
--- webforms.py
+++ webforms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField, BooleanField, DateTimeField
-from wtforms.validators import DataRequired, Length, Email, InputRequired
+from wtforms import StringField, SubmitField, PasswordField, BooleanField, DateTimeField, TextAreaField, FloatField
+from wtforms.validators import DataRequired, Length, Email, InputRequired, EqualTo
+from flask_wtf.file import FileField
 
 class LoginForm(FlaskForm):
     username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
@@ -12,3 +13,26 @@ class RegisterForm(FlaskForm):
     username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
     email = StringField('email', validators=[InputRequired(), Email(message="invalid email"), Length(max=50)])
     password = StringField('password', validators=[InputRequired(), Length(min=4, max=80)])
+
+
+class UserForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    username = StringField('username', validators=[DataRequired(), Length(4, 80)])
+    email = StringField('email', validators=[InputRequired(), Email(message="invalid email"), Length(max=50)])
+    about = TextAreaField('About Author')
+    password_hash = PasswordField('Password', validators=[DataRequired(), EqualTo('password_hash2', message='Password must match')])
+    password_hash2 = PasswordField('Confirm Password', validators=[DataRequired()])
+    profile_pic = FileField('Profile Pic')
+    submit = SubmitField("Submit")
+
+class PredictForm(FlaskForm):
+    ItemIdentifier = StringField("Item Identifier", validators=[DataRequired()])
+    Item_Weight = FloatField("Item_Weight", validators=[DataRequired()])
+    Item_Fat_Content = StringField('Item_Fat_Content', validators=[DataRequired()])
+    Item_Visibility = FloatField("Item_Visibility", validators=[DataRequired()])
+    Item_Type = StringField("Item_Type", validators=[DataRequired()])
+    Item_MRP = FloatField("Item_MRP", validators=[DataRequired()])
+    Outlet_Identifier = StringField("Outlet_Identifier", validators=[DataRequired()])
+    Outlet_Size = StringField("Outlet_Size", validators=[DataRequired()])
+    Outlet_Location_Type = StringField("Outlet_Location_Type", validators=[DataRequired()])
+    Outlet_Type = StringField("Outlet_Type", validators=[DataRequired()])
